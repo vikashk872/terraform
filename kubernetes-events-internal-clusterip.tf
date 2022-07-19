729bfcc72b3bd1996e32df0a8a0bc993389d78bb
vikashk872 <36322794+vikashk872@users.noreply.github.com>
2022-07-19 12:32:00 +0530

    Update kubernetes-events-internal-clusterip.tf

--- kubernetes-events-internal-clusterip.tf
+++ kubernetes-events-internal-clusterip.tf
@@ -1,11 +1,11 @@
-resource "kubernetes_service" "events-internal-service" {
+resource "kubernetes_service" "internal-deployment-service" {
   metadata {
-    name      = "events-internal-service"
+    name      = "internal-deployment-service"
     namespace = kubernetes_namespace.events_ns.metadata[0].name
   }
   spec {
     selector = {
-      App = kubernetes_deployment.events-internal-deployment.spec.0.template.0.metadata[0].labels.App
+      App = kubernetes_deployment.internal-deployment.spec.0.template.0.metadata[0].labels.App
     }
     port {
       port        = 8082
